acc0dcdd92c01ed2eb276990edfbe5afa5b3b0b3
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-09 23:16:14 -0400

    added modal

--- src/scenes/Home.jsx
+++ src/scenes/Home.jsx
@@ -2,14 +2,16 @@ import { Button } from "react-bootstrap"
 import Grid from "../components/MedicationGrid/Grid"
 import TestAddMedication from "../components/TestAddMedi"
 import { useNavigate } from "react-router-dom"
+import AddMed from "../components/AddMed";
 
 export default function Home(){
     const navigate = useNavigate();
 
     return(
         <>
-            <TestAddMedication />
+            {/* <TestAddMedication /> */}
             <Grid />
+            <AddMed />
             <Button onClick={() => navigate('/login')}>Logout</Button>
         </>
     )
