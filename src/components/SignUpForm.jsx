30a101e22acd3109e839dc700155b2edcddd0822
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-05-09 20:53:25 -0400

    added sign up bootstrap

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -8,23 +8,24 @@ export default function SignUpForm() {
             <h2>SignUp</h2>
             <p>Already an user? <span onClick={() => navigate('/login')}>Login!</span></p>
             <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3" >
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
+                    <Form.Text>
                     We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
+                <Form.Group className="mb-3">
+                    <Form.Check type="checkbox" label="Keep me logged in" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Login
+                <Button variant="primary" type="submit"
+                onClick={() => navigate('/home')}>
+                    Sign up
                 </Button>
             </Form>
         </Container>
